96f019073be67d7867af1b5f4039ec0a2a06626b
canthuy <canthuy140499@gmail.com>
2021-11-24 09:50:05 +0700

    refs #1 fix comment

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,19 @@ const port = 3002;
 const requestListener = function (req, res) {
   switch(req.url){
     case '/hello':
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.writeHead(200);
-        res.end(JSON.stringify("Hello World!"))
-        break;
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.writeHead(200);
+      res.end(JSON.stringify("Hello World!"))
+      break;
     default:
-        res.writeHead(404);
-        res.end(JSON.stringify("Resource not found"));
+      res.writeHead(404);
+      res.end(JSON.stringify("Resource not found"));
     }
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, ()=>{
   console.log(`Server is running on http://${host}:${port}\n`);
+
 });
