e0a16d6b06758e0433470cc9c9d843d885da4a06
canthuy <67618651+canthuy@users.noreply.github.com>
2021-11-25 15:35:38 +0700

    refs #1 init project (#2)
    * update
    
    * refs #1 init project
    
    * refs #1 init project
    
    * refs #1 fix
    
    * refs #1 fix
    
    * refs #1 fix comment
    
    * refs #1 fix add new line of end file
    
    Co-authored-by: phungMai234 <phungtuyet234@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,24 @@
 
 const http = require("http");
 const host = 'localhost';
-const port = 3001;
+const port = 3002;
 
 const requestListener = function (req, res) {
-    switch(req.url){
-        case '/hello':
-            res.setHeader('Content-Type', 'application/json');
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.writeHead(200);
-            res.end(JSON.stringify("Hello World!"))
-            break;
-        default:
-            res.writeHead(404);
-            res.end(JSON.stringify("Resource not found"));
+  switch(req.url){
+    case '/hello':
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.writeHead(200);
+      res.end(JSON.stringify("Hello World!"))
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify("Resource not found"));
     }
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, ()=>{
-    console.log('Start server!')
+  console.log(`Server is running on http://${host}:${port}\n`);
+
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import {useEffect, useState} from 'react';
+
+
 function App() {
   const [title, setTitle] = useState('')
+
   useEffect(()=>{
     const callAPI = () => {
-      fetch('http://localhost:3001/hello')
+      fetch('http://localhost:3002/hello')
         .then(response => {
            return response.json()
         })
